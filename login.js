5b2b6d30d5537309f609e8b3a62ab0ef5f899cc8
Franz <idulfranz@gmail.com>
2026-09-20 21:26:17 +0800

    Initial StaffFlow project

---
+++ login.js
@@ -0,0 +1,13 @@
+console.log("dong rene");
+
+const signup = document.getElementById("sign-up-button");
+
+signup.addEventListener("click", function () {
+  console.log("hello ");
+});
+
+const signin = document.getElementById("signin");
+
+signin.addEventListener("click", function () {
+  console.log("hi");
+});
